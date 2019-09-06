14994722d643c31f52023b2656d8fc1cb1fbb1f5
T3NSH11 <zaidsalam908@gmail.com>
2019-09-06 19:33:21 +0400

    unity files

---
+++ dance battle game/Assets/GAD170_1 Framework for Students/Assets/Scripts/StatsGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  A <see langword="static"/> class with methods (functions) for initialising or randomising the stats class.
+///  
+/// TODO:
+///     Initialise a stats instance with generated starting stats
+///     Handle the assignment of extra points or xp into an existing stats of a character
+///         - this is expected to be used by NPCs leveling up to match the player.
+/// </summary>
+public static class StatsGenerator
+{
+    public static void InitialStats(Stats stats)
+    {
+        stats.xp = 0;
+        stats.rhythm = 2;
+        stats.style = 3;
+        stats.luck = 0;
+        stats.totalstats = 0;
+
+    }
+
+    public static void AssignUnusedPoints(Stats stats, int points)
+    {
+    }
+}
---
+++ dance battle game/Assets/GAD170_1 Framework for Students/Assets/Scripts/XPHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is responsible for converting a battle result into xp to be awarded to the player.
+/// 
+/// TODO:
+///     Respond to battle outcome with xp calculation based on;
+///         player win 
+///         how strong the win was
+///         stats/levels of the dancers involved
+///     Award the calculated XP to the player stats
+///     Raise the player level up event if needed
+/// </summary>
+public class XPHandler : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        GameEvents.OnPlayerLevelUp += GameEvents_OnPlayerLevelUp;
+        GameEvents.OnBattleConclude += GameEvents_OnBattleConclude;
+    }
+
+    private void GameEvents_OnBattleConclude(BattleResultEventData data)
+    {
+        GainXP(data);
+    }
+
+
+    private void GameEvents_OnPlayerLevelUp(int level)
+    {
+       
+
+    }
+
+    private void OnDisable()
+    {
+    }
+
+    public void GainXP(BattleResultEventData data)
+    {
+        data.player.xp = data.player.xp + (int)((data.outcome * 100) / data.player.level);
+    }
+}
